0d3c0431375964020645460d1526c6d4503b89c4
gmy <>
2023-07-14 23:31:35 +0800

    commit

---
+++ apple websit/HandlingUserInput/Complete/Landmarks/Views/Helpers/FavoriteButton.swift
@@ -0,0 +1,29 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+A button that acts as a favorites indicator.
+*/
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    // @Binding类似于struct中的mutating 
+    @Binding var isSet: Bool
+
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundColor(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+struct FavoriteButton_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteButton(isSet: .constant(true))
+    }
+}
